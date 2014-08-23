f13f0dcf50114acdd7c9a7d3935a2aa8661125c6
nicsilva <nicole.allarice@gmail.com>
2014-08-23 11:12:36 -0400

    Auth tab

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('AuthCtrl', function($scope) {
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
